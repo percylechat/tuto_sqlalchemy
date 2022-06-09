9caf095752d5100b803a5446ba30c115649494d2
bussiere <bussiere@gmail.com>
2022-06-09 17:22:21 +0200

    now functionnal

--- get_facts.py
+++ get_facts.py
@@ -1,31 +1,35 @@
 import requests
 # from db_test import create_facts_table, launch_db, start_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import exc
 from datetime import datetime
 import base64
 import shutil
 from main_sqlalchemy import Fact, Image,engine,Base, Session
 
-def get_3_pics(pic_table, engine):
+def get_3_pics(engine):
     i: int = 0
     for i in range(0, 3):
-        Session = sessionmaker(bind = engine)
-        session = Session()
-        session.add(pic_table)
-        nbr_row = session.query(pic_table).count()
+        local_session=Session(bind=engine)
+        # Session = sessionmaker(bind = engine)
+        # session = Session()
+        # session.add(pic_table)
+        nbr_row = local_session.query(Image).count()
         r = requests.get("https://thiscatdoesnotexist.com/", stream=True)
         img_name : str = "cat" + str(i + nbr_row) + ".jpg"
         with open(img_name, 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
-        ins = pic_table.insert()
-        ins = pic_table.insert().values(
-        picture=img_name,
-        created_on=datetime.now(),
-        updated_on=datetime.now(),
-        )
-        conn = engine.connect()
-        result = conn.execute(ins)
+        new_img = Image(img_name = img_name)
+        local_session.add(new_img)
+        local_session.commit()
+        # ins = pic_table.insert()
+        # ins = pic_table.insert().values(
+        # picture=img_name,
+        # created_on=datetime.now(),
+        # updated_on=datetime.now(),
+        # )
+        # conn = engine.connect()
+        # result = conn.execute(ins)
         i += 1
 
 
@@ -34,11 +38,14 @@ def get_10_facts(engine):
     local_session=Session(bind=engine)
     for i in range(0, 10):
         r = requests.get("https://catfact.ninja/fact")
-        print(r.json()["fact"])
+        # print(r.json()["fact"])
         f: str = r.json()["fact"]
         new_fact = Fact(fact_content=f)
-        local_session.add(new_fact)
-        local_session.commit()
+        try:
+            local_session.add(new_fact)
+            local_session.commit()
+        except exc.SQLAlchemyError:
+            continue
         # ins = facts.insert()
         # ins = facts.insert().values(
         # fact_content=f,
@@ -57,4 +64,4 @@ if __name__ == "__main__":
     # facts = create_facts_table(engine)
     # pics = launch_db(engine)
     get_10_facts(engine)
-    get_3_pics(pics, engine)
+    get_3_pics(engine)
