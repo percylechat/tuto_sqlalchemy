76b3869416025ede9ebab453951901b41239fb06
bussiere <bussiere@gmail.com>
2022-06-09 16:58:37 +0200

    reworked sqlalchemy

--- get_facts.py
+++ get_facts.py
@@ -1,15 +1,20 @@
 import requests
-from db_test import create_facts_table, launch_db, start_engine
-# from db_test import pics as pic_table
+# from db_test import create_facts_table, launch_db, start_engine
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import base64
 import shutil
+from main_sqlalchemy import Fact, Image,engine,Base, Session
 
 def get_3_pics(pic_table, engine):
     i: int = 0
     for i in range(0, 3):
+        Session = sessionmaker(bind = engine)
+        session = Session()
+        session.add(pic_table)
+        nbr_row = session.query(pic_table).count()
         r = requests.get("https://thiscatdoesnotexist.com/", stream=True)
-        img_name = "cat" + str(i) + ".jpg"
+        img_name : str = "cat" + str(i + nbr_row) + ".jpg"
         with open(img_name, 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
@@ -24,32 +29,32 @@ def get_3_pics(pic_table, engine):
         i += 1
 
 
-def get_10_facts(facts, engine):
+def get_10_facts(engine):
     i: int = 0
+    local_session=Session(bind=engine)
     for i in range(0, 10):
         r = requests.get("https://catfact.ninja/fact")
         print(r.json()["fact"])
         f: str = r.json()["fact"]
-        ins = facts.insert()
-        ins = facts.insert().values(
-        fact_content=f,
-        created_on=datetime.now(),
-        updated_on=datetime.now(),
-        )
-        print(str(ins), ins.compile().params)
-        conn = engine.connect()
-        result = conn.execute(ins)
-        # session.
-        # result = engine.connection.execute(ins)
-        # stmt = (
-        #     facts.insert(facts).values(fact_content=f)
+        new_fact = Fact(fact_content=f)
+        local_session.add(new_fact)
+        local_session.commit()
+        # ins = facts.insert()
+        # ins = facts.insert().values(
+        # fact_content=f,
+        # created_on=datetime.now(),
+        # updated_on=datetime.now(),
         # )
+        # print(str(ins), ins.compile().params)
+        # conn = engine.connect()
+        # result = conn.execute(ins)
         i += 1
 
 
 if __name__ == "__main__":
-    engine = start_engine()
-    facts = create_facts_table(engine)
-    pics = launch_db(engine)
-    # get_10_facts(facts, engine)
+    Base.metadata.create_all(engine)
+    # engine = start_engine()
+    # facts = create_facts_table(engine)
+    # pics = launch_db(engine)
+    get_10_facts(engine)
     get_3_pics(pics, engine)
